1e49d81f59d7186be109fd2d2225bb8cca5cbd1e
Colin <cstrassergh@gmail.com>
2024-08-30 15:52:42 -0500

    added home btn toggle functionality - might revise

--- scripts/index.js
+++ scripts/index.js
@@ -2,3 +2,16 @@
 // try title.classList.toggle("article__title_theme_dark"); structure
 
 // for adding posts, use a <template> then insertAdjacentHTML()
+
+const postContent = document.querySelector(".homeContent");
+const contentHomeBtn = postContent.querySelector(".posts__home-btn");
+const cardsContent = postContent.querySelector(".cards");
+
+console.log(cardsContent.classList);
+
+function openToggle() {
+    cardsContent.classList.toggle("VisToggle");
+    console.log("works");
+}
+
+contentHomeBtn.addEventListener("click", openToggle);
