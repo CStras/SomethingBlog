a93be721491d46f02320d69358d9241565c7e29f
Colin <cstrassergh@gmail.com>
2024-08-30 14:55:33 -0500

    styling and structure changes

---
+++ scripts/index.js
@@ -0,0 +1,4 @@
+// use setAttribute to change visibility of pages
+// try title.classList.toggle("article__title_theme_dark"); structure
+
+// for adding posts, use a <template> then insertAdjacentHTML()
