a688244bb4216c394b01b5652d626ee53ca60df5
Colin <cstrassergh@gmail.com>
2024-09-10 21:48:47 -0500

    feat: add basic css to drop down menu + renaming for BEM

--- scripts/index.js
+++ scripts/index.js
@@ -9,7 +9,10 @@ contentHomeBtn.addEventListener("click", mainToggle);
 
 function menuToggle() {
     menu.classList.toggle("VisToggle");
+    menu.classList.toggle("header__drop-b4");
+    dropMenu.classList.toggle("drop__circle-b4");
 }
 const menuBtn = document.querySelector(".header__menu");
 const menu = document.querySelector(".header__drop");
+const dropMenu = menu.querySelector(".drop__circle");
 menuBtn.addEventListener("click", menuToggle);
