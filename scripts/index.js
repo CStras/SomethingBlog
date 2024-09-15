8ff3fae3bd9724b449d78425e7dd03087a115fc4
Colin <cstrassergh@gmail.com>
2024-09-15 17:25:18 -0500

    feat: add post content modal and functionality to open/close

--- scripts/index.js
+++ scripts/index.js
@@ -2,9 +2,28 @@ const postContent = document.querySelector(".homeContent");
 const contentHomeBtn = postContent.querySelector(".posts__home-btn");
 const cardsContent = postContent.querySelector(".cards");
 
+const menuBtn = document.querySelector(".header__menu");
+const menu = document.querySelector(".header__drop");
+const dropMenu = menu.querySelector(".drop__circle");
+
+const addPostModal = document.querySelector("#add-card-modal");
+const addPostBtn = document.querySelector("#add-post-btn");
+const closePostBtn = addPostModal.querySelector(".modal__close-btn");
+
+
+function openModal() {
+    addPostModal.classList.add("modal_opened");
+}
+
+function closeModal() {
+    addPostModal.classList.remove("modal_opened");
+}
+
+
 function mainToggle() {
     cardsContent.classList.toggle("VisToggle");
 }
+
 contentHomeBtn.addEventListener("click", mainToggle);
 
 function menuToggle() {
@@ -12,7 +31,9 @@ function menuToggle() {
     menu.classList.toggle("header__drop-b4");
     dropMenu.classList.toggle("drop__circle-b4");
 }
-const menuBtn = document.querySelector(".header__menu");
-const menu = document.querySelector(".header__drop");
-const dropMenu = menu.querySelector(".drop__circle");
+
 menuBtn.addEventListener("click", menuToggle);
+
+addPostBtn.addEventListener("click", openModal);
+
+closePostBtn.addEventListener("click", closeModal);
