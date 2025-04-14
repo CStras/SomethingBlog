24fde807f86ae9f12180d84fc5becfbb2d662bfb
Colin <cstrassergh@gmail.com>
2025-04-13 20:16:23 -0500

    feat: add blackClose.svg asset; update ModalWithForm styles and structure for improved layout and usability

--- src/components/ModalWithForm/ModalWithForm.jsx
+++ src/components/ModalWithForm/ModalWithForm.jsx
@@ -18,37 +18,41 @@ function ModalWithForm({
 
   return (
     <div className={`modal ${isOpen && "modal_open"}`}>
-      <div className="modal__content">
-        <h2 className="modal__title">{titleText}</h2>
-        <button
-          onClick={closeActiveModal}
-          type="button"
-          className="modal__close"
-        />
+      <div className="modal__container">
+        <div className="modal__content">
+          <h2 className="modal__title">{titleText}</h2>
+          <button
+            onClick={closeActiveModal}
+            type="button"
+            className="modal__close"
+          />
 
-        <form className="modal__form" onSubmit={handleSubmit}>
-          {children}
-          <div className="modal__next-btns">
-            {firstBtnText && (
-              <button
-                className={formValid ? "modal__submit__valid" : "modal__submit"}
-                type="submit"
-              >
-                {firstBtnText}
-              </button>
-            )}
-            {secondBtnText && (
-              <button
-                className="modal__second-btn"
-                type="button"
-                onClick={secondBtnClick}
-              >
-                <span className="modal__alt-text">or</span>
-                {secondBtnText}
-              </button>
-            )}
-          </div>
-        </form>
+          <form className="modal__form" onSubmit={handleSubmit}>
+            {children}
+            <div className="modal__next-btns">
+              {firstBtnText && (
+                <button
+                  className={
+                    formValid ? "modal__submit__valid" : "modal__submit"
+                  }
+                  type="submit"
+                >
+                  {firstBtnText}
+                </button>
+              )}
+              {secondBtnText && (
+                <button
+                  className="modal__second-btn"
+                  type="button"
+                  onClick={secondBtnClick}
+                >
+                  <span className="modal__alt-text">or</span>
+                  {secondBtnText}
+                </button>
+              )}
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   );
